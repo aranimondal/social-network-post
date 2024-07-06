4503032f57a681586e277d994b62275ff97bb6bd
Arani Mondal <147998431+aranimondal@users.noreply.github.com>
2024-07-06 13:22:30 +0530

    Update SocialNetworkPostRepository.java
    Imports added in the file

--- src/main/java/com/example/socialnetworkapi/repository/SocialNetworkPostRepository.java
+++ src/main/java/com/example/socialnetworkapi/repository/SocialNetworkPostRepository.java
@@ -1,4 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.socialnetworkapi.model.SocialNetworkPost;
+
 public interface SocialNetworkPostRepository extends JpaRepository<SocialNetworkPost, Long> {
-    List<SocialNetworkPost> findByPostCategoryOrderByViewCountDesc(String postCategory);
-    List<SocialNetworkPost> findByAuthor(String author);
+
+  List<SocialNetworkPost> findByPostCategoryOrderByViewCountDesc(String postCategory);
+
+  List<SocialNetworkPost> findByAuthor(String author);
 }
